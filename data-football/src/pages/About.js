67bf466a45ad74c44bef3f66a3e1d3fc7c7a5b68
JFKongphop <93483308+JFKongphop@users.noreply.github.com>
2022-08-27 23:47:54 +0700

    icon

--- data-football/src/pages/About.js
+++ data-football/src/pages/About.js
@@ -1,8 +1,6 @@
 // this page show about of the product or service not finish yet
 
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-
 import image from "../assets/21image.png"
 
 const About = () =>{
